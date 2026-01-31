aad9f86b43390733a58c31bb9cddd90c2501104b
Angel Morales <sr.morales0987@gmail.com>
2026-01-31 15:05:33 -0400

    Subir inicial del proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace GestionEmpleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblDirección = new System.Windows.Forms.Label();
+            this.lblTeléfono = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.lblFechaIngreso = new System.Windows.Forms.Label();
+            this.lblGénero = new System.Windows.Forms.Label();
+            this.dtpFechaIngreso = new System.Windows.Forms.DateTimePicker();
+            this.cboGenero = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(92, 61);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(92, 99);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(92, 134);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 2;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblDirección
+            // 
+            this.lblDirección.AutoSize = true;
+            this.lblDirección.Location = new System.Drawing.Point(92, 169);
+            this.lblDirección.Name = "lblDirección";
+            this.lblDirección.Size = new System.Drawing.Size(52, 13);
+            this.lblDirección.TabIndex = 3;
+            this.lblDirección.Text = "Dirección";
+            // 
+            // lblTeléfono
+            // 
+            this.lblTeléfono.AutoSize = true;
+            this.lblTeléfono.Location = new System.Drawing.Point(92, 208);
+            this.lblTeléfono.Name = "lblTeléfono";
+            this.lblTeléfono.Size = new System.Drawing.Size(49, 13);
+            this.lblTeléfono.TabIndex = 4;
+            this.lblTeléfono.Text = "Teléfono";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(92, 247);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Location = new System.Drawing.Point(92, 291);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(35, 13);
+            this.lblCargo.TabIndex = 6;
+            this.lblCargo.Text = "Cargo";
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Location = new System.Drawing.Point(92, 328);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(39, 13);
+            this.lblSalario.TabIndex = 7;
+            this.lblSalario.Text = "Salario";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(172, 61);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 8;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(172, 99);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 9;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(172, 134);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidos.TabIndex = 10;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(172, 169);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(100, 20);
+            this.txtDireccion.TabIndex = 11;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(172, 208);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefono.TabIndex = 12;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(172, 244);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.Location = new System.Drawing.Point(172, 284);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(100, 20);
+            this.txtCargo.TabIndex = 14;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(172, 325);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(100, 20);
+            this.txtSalario.TabIndex = 15;
+            // 
+            // lblFechaIngreso
+            // 
+            this.lblFechaIngreso.AutoSize = true;
+            this.lblFechaIngreso.Location = new System.Drawing.Point(92, 367);
+            this.lblFechaIngreso.Name = "lblFechaIngreso";
+            this.lblFechaIngreso.Size = new System.Drawing.Size(93, 13);
+            this.lblFechaIngreso.TabIndex = 16;
+            this.lblFechaIngreso.Text = " Fecha de Ingreso";
+            // 
+            // lblGénero
+            // 
+            this.lblGénero.AutoSize = true;
+            this.lblGénero.Location = new System.Drawing.Point(95, 401);
+            this.lblGénero.Name = "lblGénero";
+            this.lblGénero.Size = new System.Drawing.Size(42, 13);
+            this.lblGénero.TabIndex = 17;
+            this.lblGénero.Text = "Género";
+            // 
+            // dtpFechaIngreso
+            // 
+            this.dtpFechaIngreso.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaIngreso.Location = new System.Drawing.Point(191, 367);
+            this.dtpFechaIngreso.Name = "dtpFechaIngreso";
+            this.dtpFechaIngreso.Size = new System.Drawing.Size(112, 20);
+            this.dtpFechaIngreso.TabIndex = 18;
+            // 
+            // cboGenero
+            // 
+            this.cboGenero.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboGenero.FormattingEnabled = true;
+            this.cboGenero.Location = new System.Drawing.Point(172, 401);
+            this.cboGenero.Name = "cboGenero";
+            this.cboGenero.Size = new System.Drawing.Size(121, 21);
+            this.cboGenero.TabIndex = 19;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(552, 123);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 20;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(552, 159);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 21;
+            this.btnAbrir.Text = "Abrir Archivo";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(552, 198);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 22;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.cboGenero);
+            this.Controls.Add(this.dtpFechaIngreso);
+            this.Controls.Add(this.lblGénero);
+            this.Controls.Add(this.lblFechaIngreso);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.lblCargo);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTeléfono);
+            this.Controls.Add(this.lblDirección);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblDirección;
+        private System.Windows.Forms.Label lblTeléfono;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Label lblFechaIngreso;
+        private System.Windows.Forms.Label lblGénero;
+        private System.Windows.Forms.DateTimePicker dtpFechaIngreso;
+        private System.Windows.Forms.ComboBox cboGenero;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GestionEmpleados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboGenero.Items.Add("Masculino");
+            cboGenero.Items.Add("Femenino");
+        }
+
+        // 🔹 MÉTODO PARA LIMPIAR CAMPOS
+        private void LimpiarCampos()
+        {
+            txtID.Clear();
+            txtNombre.Clear();
+            txtApellidos.Clear();
+            txtDireccion.Clear();
+            txtTelefono.Clear();
+            txtEmail.Clear();
+            txtCargo.Clear();
+            txtSalario.Clear();
+
+            cboGenero.SelectedIndex = -1;
+            dtpFechaIngreso.Value = DateTime.Now;
+
+            txtID.Focus();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // VALIDACIÓN
+            if (string.IsNullOrWhiteSpace(txtID.Text) ||
+                string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                string.IsNullOrWhiteSpace(txtApellidos.Text) ||
+                string.IsNullOrWhiteSpace(txtDireccion.Text) ||
+                string.IsNullOrWhiteSpace(txtTelefono.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtCargo.Text) ||
+                string.IsNullOrWhiteSpace(txtSalario.Text) ||
+                cboGenero.SelectedIndex == -1)
+            {
+                MessageBox.Show("Debe completar todos los campos",
+                    "Validación",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            // GUARDAR ARCHIVO
+            SaveFileDialog guardar = new SaveFileDialog();
+            guardar.Filter = "Archivos de texto|*.txt";
+            guardar.Title = "Guardar datos del empleado";
+
+            if (guardar.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(guardar.FileName, true))
+                {
+                    sw.WriteLine(
+                        $"{txtID.Text}\t" +
+                        $"{txtNombre.Text}\t" +
+                        $"{txtApellidos.Text}\t" +
+                        $"{txtDireccion.Text}\t" +
+                        $"{txtTelefono.Text}\t" +
+                        $"{txtEmail.Text}\t" +
+                        $"{txtCargo.Text}\t" +
+                        $"{txtSalario.Text}\t" +
+                        $"{dtpFechaIngreso.Value.ToShortDateString()}\t" +
+                        $"{cboGenero.Text}"
+                    );
+                }
+
+                MessageBox.Show("Archivo guardado correctamente",
+                    "Éxito",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                // 🔹 LIMPIAR CAMPOS DESPUÉS DE GUARDAR
+                LimpiarCampos();
+            }
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.Filter = "Archivos de texto|*.txt";
+
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                string contenido = File.ReadAllText(abrir.FileName);
+
+                MessageBox.Show(
+                    contenido,
+                    "Datos del Empleado",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show(
+                "¿Desea salir de la aplicación?",
+                "Confirmar salida",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (r == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
